7f8087b9c0339ab319e7c11f736b1ccb69fb1218
alex <alexandre.ag.67@gmail.com>
2024-11-11 11:31:37 +0100

    Ajout des listes de heros et d'enemis + mise en place des méthodes pour le jeux

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,99 @@
+import java.util.Random;
+
 public class Main {
+
+    private static final String[][] listPlayers =
+            {
+                    {"Seong Gi-Hun", "10", "3", "1"},
+                    {"Kang Sae-byeok", "15", "2", "2"},
+                    {"Cho Sang-woo", "20", "1", "3"}
+            };
+
+    private static final String[][] listEnemies =
+            {
+                    {"Tao Huang", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Su He", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Tan Ju", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Shao Hyun-su", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Xing Kang", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Soo-Yun Chan", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Kang Liuxian", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Zhen Xiang", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Ding Bao", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Zhong Lei", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Xie Cai", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Tsing Tsao", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Cho-Hea Led", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Kim Nam-Joon", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Jeon Jung-Kook", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Ae-Cha Chul-Moo", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Sundong Xionghuo", randomNumber(1, 20), randomNumber(40, 90)},
+                    {"Li Ha-Neul", randomNumber(1, 20), randomNumber(40, 90)},
+            };
+
+    private static final String[][] listLevels =
+            {
+                    {"Facile", "Moyen", "Impossible"},
+                    {"4", "12", "18"}
+            };
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        presentHeroes(listPlayers);
+        presentEnemies(listEnemies);
+    }
+
+    private static String randomNumber(int min, int max) {
+        Random random = new Random();
+        return Integer.toString(random.nextInt(max - min + 1) +min);
+    }
+
+    private static void presentHeroes(String[][] listPlayers) {
+
+        console("------------------------------------");
+
+        for (int i = 0; i < listPlayers.length; i++) {
+            console(listPlayers[i][0] + " possède "
+                    + listPlayers[i][1] + " billes "
+                    + "avec un mallus de "
+                    + listPlayers[i][2] + " billes en cas de défaite"
+                    + " et un bonus de "
+                    + listPlayers[i][3]
+                    + " billes en cas de voictoire !");
+        }
+
+        console("------------------------------------");
+
+
+    }
+
+    private static void presentEnemies(String[][] listEnemies) {
+
+        console("------------------------------------");
+
+        for (int i = 0; i < listEnemies.length; i++) {
+            console(listEnemies[i][0] + " possède "
+                    + listEnemies[i][1] + " billes "
+                    + "et est agé de "
+                    + listEnemies[i][2] + " ans");
+        }
+
+        console("------------------------------------");
+    }
+
+    private static short readUserInput(String prompt, int min, int max) {
+        return (short) min;
+    }
+
+    private static void console(String msg) {
+        System.out.println(msg);
+    }
+
+    private static void handleEncouter(short userAnswer, short enemyMarbles, short enemyIndex) {
+
+    }
+
+    private static void startFight() {
+
     }
 }
